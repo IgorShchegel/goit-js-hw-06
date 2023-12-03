4d63c8c71bf2117325da8a3cd17d49193c8cf0fa
IgorShchegel <mitosss777@gmail.com>
2023-12-03 15:50:48 +0200

    correction js

--- js/task-2.js
+++ js/task-2.js
@@ -11,13 +11,13 @@ class Storage{
   }
 
   addItem(newItem){
-    return this.#items.push(newItem);
+     this.#items.push(newItem);
   }
 
   removeItem(itemToRemove){
     const index = this.#items.indexOf(itemToRemove);
     if(index >= 0){
-    return this.#items.splice(index,1)
+     this.#items.splice(index,1)
   }
   }
 }
